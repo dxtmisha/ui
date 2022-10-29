78f57be0f27665c5a1fd6f2520247d82448b771d
dxtmisha <dxtmisha@gmail.com>
2022-10-29 22:13:15 +0700

    + ComponentAbstract

---
+++ classes/ComponentAbstract.ts
@@ -0,0 +1,146 @@
+import {computed, ComputedRef, Ref, toRefs} from "vue";
+import {
+  AssociativeType,
+  ComponentAssociativeItemsType,
+  ComponentAssociativeType,
+  ComponentBaseType,
+  ComponentClassesType,
+  ComponentStylesType,
+  NumberOrStringType
+} from "../constructors/types";
+
+import {
+  forEach,
+  replaceRecursive,
+  toKebabCase
+} from "../functions";
+
+export abstract class ComponentAbstract {
+  protected abstract readonly design: AssociativeType
+  protected abstract readonly instruction: AssociativeType
+
+  protected readonly classesProps = [] as string[]
+
+  abstract setup(): AssociativeType
+
+  protected readonly props: AssociativeType
+  protected readonly refs: AssociativeType<Ref>
+  protected readonly context: AssociativeType
+
+  constructor(props: object, context: object) {
+    this.props = props
+    this.refs = toRefs<AssociativeType>(props)
+    this.context = context
+  }
+
+  private readonly name = computed(() => toKebabCase(this.getObjectDesign()?.[0])) as ComputedRef<string>
+  private readonly nameDesign = computed(() => toKebabCase(Object.entries(this.design)?.[0]?.[0])) as ComputedRef<string>
+  private readonly properties = computed(() => this.getObjectDesign()?.[1]) as ComputedRef<AssociativeType>
+  private readonly baseClass = computed(() => `.${this.nameDesign.value}-${this.name.value}`) as ComputedRef<string>
+
+  protected readonly classesMain = computed(() => {
+    const main = {
+      [this.getClassName()]: true
+    }
+
+    forEach(this.instruction, (instruction, name) => {
+      if (this.isPropDesign(name as string)) {
+        if (typeof this.props[name] === 'boolean') {
+          main[this.getClassName([], [name])] = this.props[name]
+        } else if (this.props[name] in this.properties.value[name]) {
+          main[this.getClassName([], [name, this.props[name]])] = true
+        }
+      }
+    })
+
+    return main
+  }) as ComputedRef<ComponentAssociativeType>
+  protected readonly classesItems = computed(() => this.getClassesItems()) as ComputedRef<ComponentAssociativeItemsType>
+  protected readonly stylesMain = computed(() => {
+    const main = {} as AssociativeType<string>
+
+    forEach(this.instruction, (instruction, name) => {
+      if (
+        this.isPropDesign(name as string) &&
+        typeof this.props[name] !== 'boolean' &&
+        !(this.props?.[name] in this.properties.value[name])
+      ) {
+        main[`--${this.getClassName([], [name])}`] = this.props[name]
+      }
+    })
+
+    return main
+  }) as ComputedRef<AssociativeType<string>>
+
+  protected baseInit(): ComponentBaseType {
+    return {
+      name: this.name,
+      nameDesign: this.nameDesign,
+      baseClass: this.baseClass
+    }
+  }
+
+  protected getClassName(
+    name = [] as string[],
+    status = [] as NumberOrStringType[]
+  ): string {
+    return `${[`${this.nameDesign.value}-${this.name.value}`, ...name].join('__')}${status.join('--')}`
+  }
+
+  getClasses(extra = {} as ComponentClassesType): ComputedRef<ComponentClassesType> {
+    return computed(() => {
+      const classes = {
+        main: this.classesMain.value,
+        ...this.classesItems.value
+      } as ComponentClassesType
+
+      return replaceRecursive(extra, classes) as ComponentClassesType
+    })
+  }
+
+  protected getClassesItems(
+    props = this.properties.value as AssociativeType,
+    parent = [] as string[]
+  ): ComponentAssociativeItemsType {
+    const classes = {} as ComponentAssociativeItemsType
+
+    forEach<AssociativeType, string, void>(props, (prop, name) => {
+      if (name.match(/^#/)) {
+        const index = name.replace(/^#/, '')
+        const names = [...parent, index]
+
+        classes[names.join('.')] = {
+          [this.getClassName(names)]: true
+        }
+
+        Object.assign(classes, this.getClassesItems(prop, names))
+      }
+    })
+
+    return classes
+  }
+
+  protected getObjectDesign(): AssociativeType {
+    return Object.entries(Object.entries(this.design)?.[0]?.[1] || {})?.[0]
+  }
+
+  getStyle(extra = {} as ComponentStylesType): ComputedRef<ComponentStylesType> {
+    return computed(() => {
+      const styles = {
+        main: this.stylesMain.value
+      }
+
+      return replaceRecursive(extra, styles) as ComponentStylesType
+    })
+  }
+
+  protected isPropDesign(name: string): boolean {
+    return (
+      this.props?.[name] &&
+      name in this.properties.value && (
+        this.classesProps.length === 0 ||
+        this.classesProps.indexOf(name) !== -1
+      )
+    )
+  }
+}
--- constructors/types.ts
+++ constructors/types.ts
@@ -1,6 +1,40 @@
+import {ComputedRef, Ref} from "vue";
+
 export type NumberOrStringType = number | string
 
 export type AssociativeType<T = any> = {
   [key: NumberOrStringType]: T
 }
 export type AssociativeOrArrayType<T = any> = T[] | AssociativeType<T> | object
+
+export type RefOrNormalType<T = any> = ComputedRef<T> | Ref<T> | T
+
+export type ComponentAssociativeValueType = (() => boolean) | boolean
+export type ComponentAssociativeRefType = RefOrNormalType<ComponentAssociativeValueType>
+export type ComponentAssociativeType = AssociativeType<ComponentAssociativeRefType>
+export type ComponentAssociativeItemsType = AssociativeType<ComponentAssociativeType>
+export type ComponentValueType = RefOrNormalType<string>
+export type ComponentBaseType = {
+  name: ComputedRef<string>
+  nameDesign: ComputedRef<string>
+  baseClass: ComputedRef<string>
+}
+export type ComponentClassesType = {
+  main: ComponentAssociativeType
+  [key: NumberOrStringType]: ComponentAssociativeType
+}
+export type ComponentStyleItemType = AssociativeType<ComponentValueType>
+export type ComponentStylesType = {
+  main: ComponentStyleItemType
+  [key: NumberOrStringType]: ComponentStyleItemType
+}
+export type ComponentItemType =
+  ComputedRef<ComponentValueType>
+  | ComputedRef<ComponentValueType[]>
+  | ComputedRef<ComponentAssociativeType>
+  | Ref<ComponentValueType>
+  | Ref<ComponentValueType[]>
+  | Ref<ComponentAssociativeType>
+  | ComponentValueType
+  | ComponentValueType[]
+  | ComponentAssociativeType
