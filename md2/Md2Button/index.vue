2987385afe2f1f28cf03e1a023e1f235073524d1
dxtmisha <dxtmisha@gmail.com>
2022-11-26 20:11:10 +0700

    Ripple: исправления

--- md2/Md2Button/index.vue
+++ md2/Md2Button/index.vue
@@ -6,15 +6,17 @@ component(
 )
   span(:class="classes.text") {{ text }}
     slot
+  md2-ripple(:inverse="appearance === 'contained'")
 </template>
 
 <script lang="ts">
+import Md2Ripple from '../Md2Ripple/index.vue'
 import { ButtonComponent } from './ButtonComponent'
 import { props } from './props'
 
 export default {
   name: 'Md2Button',
-  components: {},
+  components: { Md2Ripple },
   props,
   setup (props: object, context: object) {
     return new ButtonComponent(props, context).setup()
