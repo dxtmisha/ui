881bce9fddd265eb6396405d35ebf046a1fdfee4
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-02-14 01:08:11 +0700

    WindowComponentAbstract: обновлено

--- constructors/Window/WindowComponentAbstract.ts
+++ constructors/Window/WindowComponentAbstract.ts
@@ -1,21 +1,22 @@
-import { computed, onUnmounted, Ref, ref } from 'vue'
+import { onUnmounted } from 'vue'
 import { ComponentAbstract } from '../../classes/ComponentAbstract'
 import { EventItem } from '../../classes/EventItem'
+import { WindowClient } from './WindowClient'
+import { WindowCoordinates } from './WindowCoordinates'
+import { WindowElements } from './WindowElements'
+import { WindowEvent } from './WindowEvent'
+import { WindowOpen } from './WindowOpen'
+import { WindowOrigin } from './WindowOrigin'
+import { WindowPersistent } from './WindowPersistent'
+import { WindowPosition } from './WindowPosition'
+import { WindowStatus } from './WindowStatus'
+import { WindowVerification } from './WindowVerification'
 import { props } from './props'
 import { AssociativeType } from '../types'
 import {
   WindowClassesType,
   WindowSetupType
 } from './types'
-import { WindowElements } from './WindowElements'
-import { WindowCoordinates } from './WindowCoordinates'
-import { WindowClient } from './WindowClient'
-import { WindowPosition } from './WindowPosition'
-import { WindowOrigin } from './WindowOrigin'
-import { WindowPersistent } from './WindowPersistent'
-import { WindowStatus } from './WindowStatus'
-import { WindowOpen } from './WindowOpen'
-import { WindowEvent } from './WindowEvent'
 
 export abstract class WindowComponentAbstract extends ComponentAbstract<HTMLDivElement> {
   static readonly instruction = props as AssociativeType
@@ -34,10 +35,8 @@ export abstract class WindowComponentAbstract extends ComponentAbstract<HTMLDivE
   private readonly persistent: WindowPersistent
 
   private readonly open: WindowOpen
+  private readonly verification: WindowVerification
   private readonly event: WindowEvent
-
-  private readonly target = ref() as Ref<HTMLElement | undefined>
-  private readonly focus = computed(() => this.getTarget().closest(this.getSelector())) as Ref<HTMLElement>
 
   constructor (
     protected readonly props: AssociativeType & object,
@@ -49,7 +48,7 @@ export abstract class WindowComponentAbstract extends ComponentAbstract<HTMLDivE
 
     this.eventItem = new EventItem<void>(
       document.body,
-      async (event) => this.callbackStatus(event)
+      async (event) => this.eventCallback(event)
     )
       .setDom(this.element)
 
@@ -96,6 +95,17 @@ export abstract class WindowComponentAbstract extends ComponentAbstract<HTMLDivE
       this.refs.opening
     )
 
+    this.verification = new WindowVerification(
+      this.element,
+      this.elements,
+      this.getName(),
+      this.getSelector(),
+      this.open,
+      this.persistent,
+      this.refs.autoClose,
+      this.refs.disabled
+    )
+
     this.event = new WindowEvent(
       this.context.emit,
       this.element,
@@ -103,9 +113,7 @@ export abstract class WindowComponentAbstract extends ComponentAbstract<HTMLDivE
       this.open
     )
 
-    onUnmounted(() => {
-      this.eventItem.stop()
-    })
+    onUnmounted(() => this.eventItem.stop())
   }
 
   setup (): WindowSetupType {
@@ -137,109 +145,35 @@ export abstract class WindowComponentAbstract extends ComponentAbstract<HTMLDivE
         click: (event: MouseEvent & TouchEvent) => this.onClick(event),
         contextmenu: (event: MouseEvent & TouchEvent) => this.onContextmenu(event)
       },
-      onAnimation: () => this.onAnimation(),
-      onTransition: () => this.onTransition()
+      onAnimation: () => this.persistent.disabled(),
+      onTransition: async () => await this.open.close()
     }
   }
 
-  getStyleName (): string {
+  private getStyleName (): string {
     return `--${this.getItem().getBasicClassName()}-`
   }
 
-  private async callbackStatus (event?: Event): Promise<void> {
+  private async eventCallback (event?: Event): Promise<void> {
     if (this.open.get()) {
-      await this.verification(event?.target as HTMLElement)
+      await this.verification.is(event?.target as HTMLElement)
     } else {
       this.eventItem.stop()
     }
   }
 
-  private findControl (focus?: HTMLElement): Element | undefined {
-    return document.querySelector(`.${this.elements.getClassControl()}.${focus?.dataset.window}`) || undefined
-  }
-
-  private getTarget<R = Element> (): R {
-    return (this.target.value || this.element.value || document.body) as R
-  }
-
-  private async go (event: MouseEvent & TouchEvent): Promise<void> {
+  private async on (event: MouseEvent & TouchEvent): Promise<void> {
     this.client.set(
       EventItem.clientX(event),
       EventItem.clientY(event)
     )
 
-    await this.verification(event.target as HTMLElement)
-  }
-
-  private ifAutoClose (): boolean {
-    return this.props.autoClose &&
-      !this.getTarget().closest(`${this.elements.getByStatus('static')}, .${this.elements.getId()} .${this.elements.getClassControl()}`)
-  }
-
-  private ifChildren (target = this.getTarget() as Element): boolean {
-    const focus = target.closest<HTMLElement>(this.getSelector())
-
-    return focus !== null && (focus.dataset.window === this.elements.getId() || this.ifChildren(this.findControl(focus)))
-  }
-
-  private ifClose (): boolean {
-    return !!this.getTarget().closest(`${this.elements.getByStatus('close')}:not(${this.elements.getByStatus('static')})`)
-  }
-
-  private ifDisabled (): boolean {
-    return !this.props.disabled && !this.getTarget().closest(this.elements.getByStatus('controlStatic'))
-  }
-
-  private ifElementIsFocus (): boolean {
-    return this.element.value === this.focus.value
-  }
-
-  private ifElementIsTarget (): boolean {
-    return this.element.value === this.target.value
-  }
-
-  private ifNotBlock () {
-    return !this.getTarget().classList.contains(this.getName()) &&
-      !this.findControl(this.focus.value)?.closest(this.elements.getByStatus('block'))
-  }
-
-  async verification (target: HTMLElement) {
-    this.target.value = target
-
-    if (this.open.get()) {
-      if (this.focus.value === null) {
-        await this.open.toggle()
-      } else if (!this.ifElementIsFocus()) {
-        if (this.ifNotBlock()) {
-          if (this.ifChildren()) {
-            requestAnimationFrame(async () => {
-              if (this.focus.value?.dataset.status !== 'open') {
-                await this.open.toggle()
-              }
-            })
-          } else {
-            await this.open.toggle()
-          }
-        }
-      } else if (this.ifElementIsTarget()) {
-        if (!this.persistent.on()) {
-          await this.open.toggle()
-        }
-      } else if (
-        this.ifClose() ||
-        this.ifAutoClose() ||
-        !this.ifChildren()
-      ) {
-        await this.open.toggle()
-      }
-    } else if (this.ifDisabled()) {
-      await this.open.toggle()
-    }
+    await this.verification.is(event.target as HTMLElement)
   }
 
   private async onClick (event: MouseEvent & TouchEvent) {
     if (!this.props.contextmenu) {
-      await this.go(event)
+      await this.on(event)
     }
   }
 
@@ -248,15 +182,7 @@ export abstract class WindowComponentAbstract extends ComponentAbstract<HTMLDivE
       event.preventDefault()
       event.stopPropagation()
 
-      await this.go(event)
+      await this.on(event)
     }
-  }
-
-  private onAnimation (): void {
-    this.persistent.disabled()
-  }
-
-  private async onTransition (): Promise<void> {
-    await this.open.close()
   }
 }
---
+++ constructors/Window/WindowVerification.ts
@@ -0,0 +1,96 @@
+import { computed, Ref, ref } from 'vue'
+import { WindowElements } from './WindowElements'
+import { WindowOpen } from './WindowOpen'
+import { WindowPersistent } from './WindowPersistent'
+
+export class WindowVerification {
+  private readonly target = ref() as Ref<HTMLElement | undefined>
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor (
+    private readonly element: Ref<HTMLDivElement | undefined>,
+    private readonly elements: WindowElements,
+    private readonly name: string,
+    private readonly selector: string,
+    private readonly open: WindowOpen,
+    private readonly persistent: WindowPersistent,
+    private readonly autoClose: Ref<boolean>,
+    private readonly disabled: Ref<boolean>
+  ) {
+  }
+
+  private readonly focus = computed(() => this.getTarget().closest(this.selector)) as Ref<HTMLElement>
+
+  async is (target: HTMLElement) {
+    this.target.value = target
+
+    if (this.open.get()) {
+      if (this.focus.value === null) {
+        await this.open.toggle()
+      } else if (!this.isFocus()) {
+        if (this.isNotBlock()) {
+          if (this.isChildren()) {
+            requestAnimationFrame(async () => {
+              if (this.focus.value?.dataset.status !== 'open') {
+                await this.open.toggle()
+              }
+            })
+          } else {
+            await this.open.toggle()
+          }
+        }
+      } else if (this.isTarget()) {
+        if (!this.persistent.on()) {
+          await this.open.toggle()
+        }
+      } else if (
+        this.isClose() ||
+        this.isAutoClose() ||
+        !this.isChildren()
+      ) {
+        await this.open.toggle()
+      }
+    } else if (this.isEnabled()) {
+      await this.open.toggle()
+    }
+  }
+
+  private getTarget<R = Element> (): R {
+    return (this.target.value || this.element.value || document.body) as R
+  }
+
+  private isFocus (): boolean {
+    return this.element.value === this.focus.value
+  }
+
+  private isTarget (): boolean {
+    return this.element.value === this.target.value
+  }
+
+  private isEnabled (): boolean {
+    return !this.disabled.value && !this.getTarget().closest(this.elements.getByStatus('controlStatic'))
+  }
+
+  private isAutoClose (): boolean {
+    return this.autoClose.value &&
+      !this.getTarget().closest(`${this.elements.getByStatus('static')}, .${this.elements.getId()} .${this.elements.getClassControl()}`)
+  }
+
+  private isClose (): boolean {
+    return !!this.getTarget().closest(`${this.elements.getByStatus('close')}:not(${this.elements.getByStatus('static')})`)
+  }
+
+  private isChildren (target = this.getTarget() as Element): boolean {
+    const focus = target.closest<HTMLElement>(this.selector)
+    return focus !== null && (focus.dataset.window === this.elements.getId() || this.isChildren(this.findControl(focus)))
+  }
+
+  private isNotBlock () {
+    return !this.getTarget().classList.contains(this.name) &&
+      !this.findControl(this.focus.value)?.closest(this.elements.getByStatus('block'))
+  }
+
+  private findControl (focus?: HTMLElement): Element | undefined {
+    return document.querySelector(`.${this.elements.getClassControl()}.${focus?.dataset.window}`) || undefined
+  }
+}
