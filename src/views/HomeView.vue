ec1ad03ca62fe0413fb1927522631b503709e6eb
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-02-20 01:18:19 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -113,6 +113,70 @@
       <md2-input appearance="outlined" counter :maxlength="12" text="counter + maxlength"/>
     </div>
 
+    <div class="px-4 py-2"><b>input, standard</b></div>
+    <div class="flex px-4 py-2 gap-2">
+      <md2-input v-model="inputValue" appearance="standard" text="This selector could have also been written as"/>
+      <md2-input v-model="inputValue" appearance="standard" palette="green" text="green"/>
+      <md2-input
+        v-model="inputValue"
+        appearance="standard"
+        palette="green"
+        text="green + validation"
+        validation-message="validation message"
+      />
+      <md2-input v-model="inputValue" appearance="standard" palette="green" selected text="green + selected"/>
+    </div>
+    <div class="flex px-4 py-2 gap-2">
+      <md2-input appearance="standard" text="text + value" value="value"/>
+      <md2-input appearance="standard" text="validation" validation-message="validation message"/>
+      <md2-input appearance="standard" selected text="selected"/>
+      <md2-input appearance="standard" selected text="validation + selected" validation-message="validation message"/>
+    </div>
+    <div class="flex px-4 py-2 gap-2">
+      <md2-input v-model="inputValue" appearance="standard" disabled text="disabled"/>
+      <md2-input
+        v-model="inputValue"
+        appearance="standard"
+        disabled
+        text="disabled + validation"
+        validation-message="validation message"
+      />
+      <md2-input v-model="inputValue" appearance="standard" disabled selected text="disabled + selected"/>
+      <md2-input
+        v-model="inputValue"
+        appearance="standard"
+        disabled
+        selected
+        text="disabled + validation + selected"
+        validation-message="validation message"
+      />
+    </div>
+    <div class="flex px-4 py-2 gap-2">
+      <md2-input v-model="inputValue" appearance="standard" readonly text="readonly"/>
+      <md2-input
+        v-model="inputValue"
+        appearance="standard"
+        readonly
+        text="readonly + validation"
+        validation-message="validation message"
+      />
+      <md2-input v-model="inputValue" appearance="standard" readonly selected text="readonly + selected"/>
+      <md2-input
+        v-model="inputValue"
+        appearance="standard"
+        readonly
+        selected
+        text="readonly + validation + selected"
+        validation-message="validation message"
+      />
+    </div>
+    <div class="flex px-4 py-2 gap-2">
+      <md2-input appearance="standard" required text="required"/>
+      <md2-input appearance="standard" helper-message="helper message" text="helper"/>
+      <md2-input appearance="standard" counter text="counter"/>
+      <md2-input appearance="standard" counter :maxlength="12" text="counter + maxlength"/>
+    </div>
+
     <div class="px-4 pt-4 pb-2"><b>input, icon</b></div>
     <div class="flex px-4 py-2 gap-2">
       <md2-input icon="face" text="text"/>
@@ -127,6 +191,14 @@
       <md2-input appearance="outlined" icon="face" text="text + value" value="value"/>
       <md2-input appearance="outlined" icon="face" text="validation" validation-message="validation message"/>
       <md2-input appearance="outlined" disabled icon="face" text="disabled"/>
+    </div>
+
+    <div class="px-4 pt-4 pb-2"><b>input, standard, icon</b></div>
+    <div class="flex px-4 py-2 gap-2">
+      <md2-input appearance="standard" icon="face" text="text"/>
+      <md2-input appearance="standard" icon="face" text="text + value" value="value"/>
+      <md2-input appearance="standard" icon="face" text="validation" validation-message="validation message"/>
+      <md2-input appearance="standard" disabled icon="face" text="disabled"/>
     </div>
 
     <div class="px-4 pt-4 pb-2"><b>input, icon-trailing</b></div>
