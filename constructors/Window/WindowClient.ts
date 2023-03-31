71d7c791ad1e400617014af4f750238cee0b3b48
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-04-01 03:47:57 +0700

    Update WindowClient.ts

--- constructors/Window/WindowClient.ts
+++ constructors/Window/WindowClient.ts
@@ -1,25 +1,61 @@
+/**
+ * The class stores the coordinates of the mouse click. It is used for the opening
+ * animation, when the element appears from the point of click. And also for the contextmenu event
+ *
+ * Класс хранит координаты нажатия мыши. Используется для анимации открытия,
+ * когда элемент появляется от точки нажатия. А также для события contextmenu
+ */
 export class WindowClient {
   private x = 0 as number
   private y = 0 as number
 
-  getX () {
+  /**
+   * Returns the X coordinate
+   *
+   * Возвращает координату X
+   */
+  getX (): number {
     return this.x
   }
 
-  getY () {
+  /**
+   * Returns the Y coordinate
+   *
+   * Возвращает координату Y
+   */
+  getY (): number {
     return this.y
   }
 
-  getShiftX (value: number): number | null {
-    return this.x ? this.x - value : null
+  /**
+   * Returns the shift along the X coordinate
+   *
+   * Возвращает сдвиг по координате X
+   */
+  getShiftX (value: number): number {
+    return this.x - value
   }
 
-  getShiftY (value: number): number | null {
-    return this.y ? this.y - value : null
+  /**
+   * Returns the shift along the Y coordinate
+   *
+   * Возвращает сдвиг по координате Y
+   */
+  getShiftY (value: number): number {
+    return this.y - value
   }
 
-  set (x: number, y: number) {
+  /**
+   * Changes the coordinates
+   *
+   * Изменяет координаты
+   * @param x value of X / значение X
+   * @param y value of Y / значение Y
+   */
+  set (x: number, y: number): this {
     this.x = x
     this.y = y
+
+    return this
   }
 }
