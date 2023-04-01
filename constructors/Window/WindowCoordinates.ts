ff85fedda43df470cf5c35d22b6addf11708a92a
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-04-01 10:42:02 +0700

    Update WindowCoordinates.ts

--- constructors/Window/WindowCoordinates.ts
+++ constructors/Window/WindowCoordinates.ts
@@ -2,7 +2,20 @@ import { Ref } from 'vue'
 import { WindowElements } from './WindowElements'
 import { WindowCoordinatesType } from './types'
 
+/**
+ * The object stores parameters for the control element. The position and size of
+ * the menu are calculated based on these data
+ *
+ * Объект хранит параметры для элемента управления. По этим данным вычисляются
+ * положение и размер меню
+ */
 export class WindowCoordinates {
+  /**
+   * Data with parameters
+   *
+   * Данных с параметрами
+   * @private
+   */
   private readonly item = {
     top: 0,
     right: 0,
@@ -21,34 +34,74 @@ export class WindowCoordinates {
   ) {
   }
 
+  /**
+   * Returns parameters
+   *
+   * Возвращает параметры
+   */
   get (): WindowCoordinatesType {
     return this.item
   }
 
+  /**
+   * Returns the right margin
+   *
+   * Возвращает отступ сверху
+   */
   getTop (): number {
     return this.item.top
   }
 
+  /**
+   * Returns the right margin
+   *
+   * Возвращает отступ справа
+   */
   getRight (): number {
     return this.item.right
   }
 
+  /**
+   * Returns the bottom margin
+   *
+   * Возвращает отступ снизу
+   */
   getBottom (): number {
     return this.item.bottom
   }
 
+  /**
+   * Returns the left margin
+   *
+   * Возвращает отступ слева
+   */
   getLeft (): number {
     return this.item.left
   }
 
+  /**
+   * Returns the width
+   *
+   * Возвращает ширину
+   */
   getWidth (): number {
     return this.item.width
   }
 
+  /**
+   * Returns the height
+   *
+   * Возвращает высоту
+   */
   getHeight (): number {
     return this.item.height
   }
 
+  /**
+   * Updates the settings
+   *
+   * Обновляет параметры
+   */
   update (): boolean {
     const rect = this.elements.getRect()
 
@@ -80,6 +133,11 @@ export class WindowCoordinates {
     return false
   }
 
+  /**
+   * Resets the settings. Used to enumerate coordinates when opening
+   *
+   * Сбрасывает параметры. Используется для перечисления координат при открытии
+   */
   restart (): this {
     this.item.top = 0
     this.item.right = 0
